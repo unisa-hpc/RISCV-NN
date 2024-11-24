31b20fed53dbf704233413ca4707234d00f0e00f
saleh <salehjamaligolzar@gmail.com>
2024-11-24 13:26:06 +0100

    Added progress report with echo and `/tmp/progressBenchIdXX.txt` to runme_plots.sh .

--- benchmarks/amd64/03/runme_plots.sh
+++ benchmarks/amd64/03/runme_plots.sh
@@ -14,23 +14,32 @@ bash build.amd64.00.sh -d # wipe the dumps dir.
 input_size_vals=(512 1024)
 ch_in_vals=(16 256 1024)
 ch_out_vals=(16 256 1024)
+kernel_vals=(3 5 7)
+stride_vals=(1 2)
 unroll0_vals=(1)
 unroll1_vals=(1 2)
 unroll2_vals=(1 3 5 7)
 unroll3_vals=(1 3 5 7)
 
+index=0
+total_benchmarks=$(( ${#input_size_vals[@]} * ${#ch_in_vals[@]} * ${#ch_out_vals[@]} * ${#kernel_vals[@]} * ${#stride_vals[@]} * ${#unroll0_vals[@]} * ${#unroll1_vals[@]} * ${#unroll2_vals[@]} * ${#unroll3_vals[@]} ))
+
 for input_width in "${input_size_vals[@]}"; do
   for input_height in "${input_size_vals[@]}"; do
-    for ((kernel_size=3; kernel_size<=7; kernel_size+=2)); do
+    for kernel_size in "${kernel_vals[@]}"; do
       for ch_in in "${ch_in_vals[@]}"; do
         for ch_out in "${ch_out_vals[@]}"; do
-          for stride in 1 2; do
+          for stride in "${stride_vals[@]}"; do
             for u0 in "${unroll0_vals[@]}"; do
               for u1 in "${unroll1_vals[@]}"; do
                 for u2 in "${unroll2_vals[@]}"; do
                   for u3 in "${unroll3_vals[@]}"; do
+                    index=$((index+1))
+                    echo "*** benchmark $index out of $total_benchmarks (percent: $((index*100/total_benchmarks))%)"
                     echo "Benchmarking for Input Width of $input_width, Input Height of $input_height, Kernel Size of $kernel_size, Channels In of $ch_in, Channels Out of $ch_out, Stride of $stride, Unroll Factors of $u0, $u1, $u2, $u3."
                     bash build.amd64.00.sh "-DI_H=$input_height -DI_W=$input_width -DK_H=$kernel_size -DK_W=$kernel_size -DC_I=$ch_in -DC_O=$ch_out -DS_X=$stride -DS_Y=$stride -DUNROLL_FACTOR0=$u0 -DUNROLL_FACTOR1=$u1 -DUNROLL_FACTOR2=$u2 -DUNROLL_FACTOR3=$u3 ${optional_flags}"
+                    # save progress into a text file in /tmp including the percentage of completion and all the parameters with a header line
+                    echo "Percent: $((index*100/total_benchmarks))%, Input Width: $input_width, Input Height: $input_height, Kernel Size: $kernel_size, Channels In: $ch_in, Channels Out: $ch_out, Stride: $stride, Unroll Factors: $u0, $u1, $u2, $u3" >> /tmp/progressBenchId03.txt
                   done
                 done
               done
