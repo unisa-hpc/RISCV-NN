07e0e01c558aff3fc3f8ac765eee80f8ce035654
saleh <salehjamaligolzar@gmail.com>
2025-01-28 17:02:27 +0100

    Added section to hardcode the compilers to be used in `amd64/run_all.sh`.

--- benchmarks/amd64/run_all.sh
+++ benchmarks/amd64/run_all.sh
@@ -2,13 +2,15 @@
 set -e
 
 # Check if there is at least one argument supplied
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 machine_name compiler_exec"
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 machine_name"
     exit 1
 fi
 
 machine=$1
-compiler=$2
+
+# Array of compiler executable names
+compilers=("g++" "clang++-17")
 
 # Prompt user about deleting the dumps directory
 read -p "Do you want to delete the related sub-dumps directories related to each benchId? (y/n): " delete_dumps_input
@@ -23,20 +25,23 @@ pip install --user argparse pandas colorama pathlib matplotlib numpy seaborn
 # Function to process each benchmark
 process_benchmark() {
     local bench_id=$1
-    echo "Processing benchmark $bench_id..."
+    local compiler=$2
+    echo "Processing benchmark $bench_id with compiler $compiler..."
     cd $bench_id
     if [ "$delete_dumps" = true ]; then
         echo "Deleting dumps for benchmark $bench_id..."
         bash runme.sh --machine=$machine "$compiler" -d
     fi
-    echo "Running auto-tune for benchmark $bench_id..."
+    echo "Running auto-tune for benchmark $bench_id with compiler $compiler..."
     bash runme.sh --machine=$machine "$compiler" --auto-tune
-    echo "Running final compilation for benchmark $bench_id..."
+    echo "Running final compilation for benchmark $bench_id with compiler $compiler..."
     bash runme.sh --machine=$machine "$compiler"
     cd ..
 }
 
 # Process each benchmark
 for bench_id in 2 7 8; do
-    process_benchmark $bench_id
+    for compiler in "${compilers[@]}"; do
+        process_benchmark $bench_id "$compiler"
+    done
 done
