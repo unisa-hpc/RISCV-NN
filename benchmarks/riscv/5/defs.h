1a8c0b08afcd998011cd2894d85d3210da22f9fc
saleh <salehjamaligolzar@gmail.com>
2025-01-31 21:38:55 +0100

    Fxied the missing defs in benchId5 in riscv.

--- benchmarks/riscv/5/defs.h
+++ benchmarks/riscv/5/defs.h
@@ -49,3 +49,17 @@ constexpr int UNROLL_FACTOR2_DEFAULT = 1;
 #define UNROLL_FACTOR1_BASELINE UNROLL_FACTOR1_DEFAULT
 #define UNROLL_FACTOR2_BASELINE UNROLL_FACTOR2_DEFAULT
 #endif
+
+// This is just a flag, so we can separate the best config runs from best config runs with the default unrolling factors
+// to compute the gain we get from autotuning, in the pyhton plotting scripts. It does not affect any of the unrolling factors values.
+#ifdef AUTOTUNE_IS_DISABLED
+#define FLAG_AUTOTUNE_DISABLED 1
+#else
+#define FLAG_AUTOTUNE_DISABLED 0
+#endif
+
+#ifndef ONLY_RUN_OURS
+#define RUN_BASELINES 1
+#else
+#define RUN_BASELINES 0
+#endif
