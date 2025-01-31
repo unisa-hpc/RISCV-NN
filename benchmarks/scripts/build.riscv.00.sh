d21a1a9f5dee463a48ea4e0f7008544802b7d2d2
saleh <salehjamaligolzar@gmail.com>
2025-01-31 14:51:35 +0100

    Added `-ffast-math` to all targets. Added `zvl256b` to `-march` of riscv.

--- benchmarks/scripts/build.riscv.00.sh
+++ benchmarks/scripts/build.riscv.00.sh
@@ -25,16 +25,16 @@ function set_compiler_flags() {
     # Check is_gcc_flag and is_clang_flag
     if [[ "$is_gcc_flag" -eq 1 ]]; then
         echo "Using G++ compatible flags."
-        flags_main="-O3 -march=rv64gcv -fno-tree-vectorize -fno-tree-slp-vectorize ${new_dump_dir}/libvec.a ${new_dump_dir}/libscalarvec.a ${new_dump_dir}/libscalarnovec.a -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
-        flags_vec="-c -O3 -march=rv64gcv -fno-tree-vectorize -fno-tree-slp-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
-        flags_scalar_vec="-c -O3 -march=rv64gcv -DAUTOVEC -fopt-info-vec-all -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
-        flags_scalar_novec="-c -O3 -march=rv64gcv -fno-tree-vectorize -fno-tree-slp-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_main="-O3 -march=rv64gcv_zvl256b -ffast-math -fno-tree-vectorize -fno-tree-slp-vectorize ${new_dump_dir}/libvec.a ${new_dump_dir}/libscalarvec.a ${new_dump_dir}/libscalarnovec.a -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_vec="-c -O3 -march=rv64gcv_zvl256b -ffast-math -fno-tree-vectorize -fno-tree-slp-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_scalar_vec="-c -O3 -march=rv64gcv_zvl256b -ffast-math -DAUTOVEC -fopt-info-vec-all -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_scalar_novec="-c -O3 -march=rv64gcv_zvl256b -ffast-math -fno-tree-vectorize -fno-tree-slp-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
     elif [[ "$is_clang_flag" -eq 1 ]]; then
         echo "Using Clang++ compatible flags."
-        flags_main="-O3 -march=rv64gcv -fno-vectorize ${new_dump_dir}/libvec.a ${new_dump_dir}/libscalarvec.a ${new_dump_dir}/libscalarnovec.a -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
-        flags_vec="-c -O3 -march=rv64gcv -fno-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
-        flags_scalar_vec="-c -O3 -march=rv64gcv -DAUTOVEC -fvectorize -Rpass=loop-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
-        flags_scalar_novec="-c -O3 -march=rv64gcv -fno-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_main="-O3 -march=rv64gcv_zvl256b -ffast-math -fno-vectorize ${new_dump_dir}/libvec.a ${new_dump_dir}/libscalarvec.a ${new_dump_dir}/libscalarnovec.a -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_vec="-c -O3 -march=rv64gcv_zvl256b -ffast-math -fno-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_scalar_vec="-c -O3 -march=rv64gcv_zvl256b -ffast-math -DAUTOVEC -fvectorize -Rpass=loop-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
+        flags_scalar_novec="-c -O3 -march=rv64gcv_zvl256b -ffast-math -fno-vectorize -Wall -Wextra -v -I$script_dir/../../common $extra_flags"
     else
         echo "Error: Unrecognized compiler '$compiler_path'. Must be g++ or clang++ (with optional version number)"
         exit 1
