f819ca5795546578cf6a5f9176662dce39d18eee
PGG106 <giuseppepagano10@gmail.com>
2025-03-08 18:06:29 +0100

    final touch

--- benchmarks/amd64/10/main.cpp
+++ benchmarks/amd64/10/main.cpp
@@ -62,12 +62,11 @@ int main(int argc, char **argv)
     c_tensor_avx5_mul.wipe();
     a_tensor.initialize(aligned_tensor<float>::init_type::random, -10000.f, 10000.f);
 
-    for
-        int(i = 0 : i < a_tensor.size; i++)
-        {
-            if (i % 2)
-                a_tensor.data()[i] = std::numeric_limits<float>::infinity();
-        }
+    for (int i = 0; i < a_tensor.sizet(); i++)
+    {
+        if (i % 2)
+            a_tensor.data_t()[i] = std::numeric_limits<float>::infinity();
+    }
 
     auto *a_ptr = a_tensor.data_t();
     auto *b_ptr = b_tensor.data_t();
