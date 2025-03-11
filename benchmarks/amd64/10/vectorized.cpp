2ca8fb55658ddbf3f2555d568d39f64d9e5e0621
PGG106 <giuseppepagano10@gmail.com>
2025-03-11 21:35:03 +0100

    manually unroll loop

--- benchmarks/amd64/10/vectorized.cpp
+++ benchmarks/amd64/10/vectorized.cpp
@@ -132,26 +132,78 @@ void avx512_matmul_floatbitmanipu_nopack_float_uint8(
                 // Load the main vector B
                 const uint8_t *ptr_b = b + i * N + k; // `b` is column major
                 __m512i vec_b = _mm512_load_si512(reinterpret_cast<const __m512i *>(ptr_b));
-                for (int t = 0; t < 4; t++)
-                {
-                    // start the splitting process. 4 vectors of 128 bits each, converted to 4 vectors of 512bits each
-                    auto b1 = _mm512_cvtepi8_epi32(_mm512_extracti64x2_epi64(vec_b, t));
-                    // 1st Pass
-                    const float *ptr_a = a + j * N + k + 16 * t; // `a` is row major
-                    __m512 vec_a = _mm512_load_ps(reinterpret_cast<const __m512i *>(ptr_a));
-                    const auto infinity_mask = is_not_infinity(vec_a);
-                    auto vec_a_int = _mm512_castps_si512(vec_a);
-                    auto negative_mask = _mm512_cmpgt_epi32_mask(b1, negative_detection);
-                    b1 = _mm512_mask_sub_epi32(b1, negative_mask, b1, sign_removal);
-                    // shift vec_b to line up the exponents
-                    auto shifted_vec = _mm512_slli_epi32(b1, 23);
-                    // add exponents
-                    __m512i vec_sum = _mm512_mask_add_epi32(vec_a_int, infinity_mask, vec_a_int, shifted_vec);
-                    vec_sum = _mm512_mask_xor_epi32(vec_sum, negative_mask, vec_sum, sign_vec);
-                    // accumulate
-                    __m512 vec_f = _mm512_castsi512_ps(vec_sum);
-                    vec_s = _mm512_add_ps(vec_s, vec_f);
-                }
+
+                // start the splitting process. 4 vectors of 128 bits each, converted to 4 vectors of 512bits each
+                auto b1 = _mm512_cvtepi8_epi32(_mm512_extracti64x2_epi64(vec_b, 0));
+                // 1st Pass
+                const float *ptr_a = a + j * N + k; // `a` is row major
+                __m512 vec_a = _mm512_load_ps(reinterpret_cast<const __m512i *>(ptr_a));
+                auto infinity_mask = is_not_infinity(vec_a);
+                auto vec_a_int = _mm512_castps_si512(vec_a);
+                auto negative_mask = _mm512_cmpgt_epi32_mask(b1, negative_detection);
+                b1 = _mm512_mask_sub_epi32(b1, negative_mask, b1, sign_removal);
+                // shift vec_b to line up the exponents
+                auto shifted_vec = _mm512_slli_epi32(b1, 23);
+                // add exponents
+                __m512i vec_sum = _mm512_mask_add_epi32(vec_a_int, infinity_mask, vec_a_int, shifted_vec);
+                vec_sum = _mm512_mask_xor_epi32(vec_sum, negative_mask, vec_sum, sign_vec);
+                // accumulate
+                __m512 vec_f = _mm512_castsi512_ps(vec_sum);
+                vec_s = _mm512_add_ps(vec_s, vec_f);
+
+                // start the splitting process. 4 vectors of 128 bits each, converted to 4 vectors of 512bits each
+                b1 = _mm512_cvtepi8_epi32(_mm512_extracti64x2_epi64(vec_b, 1));
+                // 2nd Pass
+                ptr_a = a + j * N + k + 16; // `a` is row major
+                vec_a = _mm512_load_ps(reinterpret_cast<const __m512i *>(ptr_a));
+                infinity_mask = is_not_infinity(vec_a);
+                vec_a_int = _mm512_castps_si512(vec_a);
+                negative_mask = _mm512_cmpgt_epi32_mask(b1, negative_detection);
+                b1 = _mm512_mask_sub_epi32(b1, negative_mask, b1, sign_removal);
+                // shift vec_b to line up the exponents
+                shifted_vec = _mm512_slli_epi32(b1, 23);
+                // add exponents
+                vec_sum = _mm512_mask_add_epi32(vec_a_int, infinity_mask, vec_a_int, shifted_vec);
+                vec_sum = _mm512_mask_xor_epi32(vec_sum, negative_mask, vec_sum, sign_vec);
+                // accumulate
+                vec_f = _mm512_castsi512_ps(vec_sum);
+                vec_s = _mm512_add_ps(vec_s, vec_f);
+
+                // start the splitting process. 4 vectors of 128 bits each, converted to 4 vectors of 512bits each
+                b1 = _mm512_cvtepi8_epi32(_mm512_extracti64x2_epi64(vec_b, 2));
+                // 3rd Pass
+                ptr_a = a + j * N + k + 32; // `a` is row major
+                vec_a = _mm512_load_ps(reinterpret_cast<const __m512i *>(ptr_a));
+                infinity_mask = is_not_infinity(vec_a);
+                vec_a_int = _mm512_castps_si512(vec_a);
+                negative_mask = _mm512_cmpgt_epi32_mask(b1, negative_detection);
+                b1 = _mm512_mask_sub_epi32(b1, negative_mask, b1, sign_removal);
+                // shift vec_b to line up the exponents
+                shifted_vec = _mm512_slli_epi32(b1, 23);
+                // add exponents
+                vec_sum = _mm512_mask_add_epi32(vec_a_int, infinity_mask, vec_a_int, shifted_vec);
+                vec_sum = _mm512_mask_xor_epi32(vec_sum, negative_mask, vec_sum, sign_vec);
+                // accumulate
+                vec_f = _mm512_castsi512_ps(vec_sum);
+                vec_s = _mm512_add_ps(vec_s, vec_f);
+
+                // start the splitting process. 4 vectors of 128 bits each, converted to 4 vectors of 512bits each
+                b1 = _mm512_cvtepi8_epi32(_mm512_extracti64x2_epi64(vec_b, 3));
+                // 4th Pass
+                ptr_a = a + j * N + k + 48; // `a` is row major
+                vec_a = _mm512_load_ps(reinterpret_cast<const __m512i *>(ptr_a));
+                infinity_mask = is_not_infinity(vec_a);
+                vec_a_int = _mm512_castps_si512(vec_a);
+                negative_mask = _mm512_cmpgt_epi32_mask(b1, negative_detection);
+                b1 = _mm512_mask_sub_epi32(b1, negative_mask, b1, sign_removal);
+                // shift vec_b to line up the exponents
+                shifted_vec = _mm512_slli_epi32(b1, 23);
+                // add exponents
+                vec_sum = _mm512_mask_add_epi32(vec_a_int, infinity_mask, vec_a_int, shifted_vec);
+                vec_sum = _mm512_mask_xor_epi32(vec_sum, negative_mask, vec_sum, sign_vec);
+                // accumulate
+                vec_f = _mm512_castsi512_ps(vec_sum);
+                vec_s = _mm512_add_ps(vec_s, vec_f);
             }
 
             float sum = _mm512_reduce_add_ps(vec_s);
