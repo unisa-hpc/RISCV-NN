1eebe090c42c71e396daa4b73a2ace45afdf95b0
saleh <salehjamaligolzar@gmail.com>
2025-03-11 23:12:06 +0100

    Fixed runme.sh for bid10. Added lambda support for bid10 to lamda_funcs.py

--- benchmarks/amd64/10/runme.sh
+++ benchmarks/amd64/10/runme.sh
@@ -1,4 +1,4 @@
 set -e
-current_benchId="7"
+current_benchId="10"
 source "../../common/runme.matmul.any.sh"
 run_benchmark $current_benchId "amd64" "$@"
--- benchmarks/common/python/parsing/lamda_funcs.py
+++ benchmarks/common/python/parsing/lamda_funcs.py
@@ -3,17 +3,14 @@ def get_tunable_params_list(bench_id: int):
     if not isinstance(bench_id, int):
         raise ValueError(f'bench_id should be an integer, got {bench_id}')
     if bench_id == 0:
-        print(f'The given bench_id of {bench_id} is does not need any tunable parameters (get_tunable_params).')
-        return None
-    elif bench_id == 1 or bench_id == 2 or bench_id == 5 or bench_id == 6 or bench_id == 7 or bench_id == 8:
+        raise ValueError(f'The given bench_id of {bench_id} is does not need any tunable parameters (get_tunable_params).')
+    elif bench_id == 1 or bench_id == 2 or bench_id == 5 or bench_id == 6 or bench_id == 7 or bench_id == 8 or bench_id == 10:
         return ['UNROLL_FACTOR0', 'UNROLL_FACTOR1', 'UNROLL_FACTOR2']
     elif bench_id == 4:
-        print(f'The given bench_id of {bench_id} is does not need any tunable parameters (get_tunable_params).')
-        return None
+        raise ValueError(f'The given bench_id of {bench_id} is does not need any tunable parameters (get_tunable_params).')
     else:
         # benchID 3 also falls here
-        print(f'The given bench_id of {bench_id} is defined yet (get_tunable_params).')
-        return None
+        raise ValueError(f'The given bench_id of {bench_id} is not defined yet (get_tunable_params).')
 
 def get_lambda_pairs(bench_id: int):
     # 0X, 1MM, 2MM, 3CONV, 4CUDA, 5MM, 6MM, 7MM, 8MM
@@ -21,9 +18,8 @@ def get_lambda_pairs(bench_id: int):
     if not isinstance(bench_id, int):
         raise ValueError(f'bench_id should be an integer, got {bench_id}')
     if bench_id == 0:
-        print(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
-        return None
-    elif bench_id == 1 or bench_id == 2 or bench_id == 5 or bench_id == 6 or bench_id == 7 or bench_id == 8:
+        raise ValueError(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
+    elif bench_id == 1 or bench_id == 2 or bench_id == 5 or bench_id == 6 or bench_id == 7 or bench_id == 8 or bench_id == 10:
         return lambda pairs: {
             'FLAG_AUTOTUNE_DISABLED': pairs['FLAG_AUTOTUNE_DISABLED'],
             'N': pairs['N'],
@@ -32,12 +28,10 @@ def get_lambda_pairs(bench_id: int):
             'UNROLL_FACTOR2': pairs['UNROLL_FACTOR2']
         }
     elif bench_id == 4:
-        print(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
-        return None
+        raise ValueError(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
     else:
         # benchID 3 also falls here
-        print(f'The given bench_id of {bench_id} is defined yet (get_lambda_pairs).')
-        return None
+        raise ValueError(f'The given bench_id of {bench_id} is not defined yet (get_lambda_pairs).')
 
 
 def get_lambda_parse_pairs(bench_id: int):
@@ -45,18 +39,15 @@ def get_lambda_parse_pairs(bench_id: int):
     if not isinstance(bench_id, int):
         raise ValueError(f'bench_id should be an integer, got {bench_id}')
     if bench_id == 0:
-        print(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
-        return None
-    elif bench_id == 1 or bench_id == 2 or bench_id == 5 or bench_id == 6 or bench_id == 7 or bench_id == 8:
+        raise ValueError(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
+    elif bench_id == 1 or bench_id == 2 or bench_id == 5 or bench_id == 6 or bench_id == 7 or bench_id == 8 or bench_id == 10:
         return lambda pairs, hw_name: \
             'name=' + pairs['name'] + f';;hw={hw_name}' + ';;N=' + str(pairs['N']) + \
             ';;UNROLL_FACTOR0=' + str(pairs['UNROLL_FACTOR0']) + \
             ';;UNROLL_FACTOR1=' + str(pairs['UNROLL_FACTOR1']) + \
             ';;UNROLL_FACTOR2=' + str(pairs['UNROLL_FACTOR2'])
     elif bench_id == 4:
-        print(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
-        return None
+        raise ValueError(f'The given bench_id of {bench_id} is does not need any pairs lambda (get_lambda_pairs).')
     else:
         # benchID 3 also falls here
-        print(f'The given bench_id of {bench_id} is defined yet (get_lambda_pairs).')
-        return None
+        raise ValueError(f'The given bench_id of {bench_id} is not defined yet (get_lambda_pairs).')
